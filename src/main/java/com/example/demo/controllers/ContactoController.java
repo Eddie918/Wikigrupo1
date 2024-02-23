3a17a5df81db1ef299770ea3786f4bcaa086f193
Jerson Osorio <61725276+Velorio@users.noreply.github.com>
2024-02-23 13:52:57 -0500

    commit

--- src/main/java/com/example/demo/controllers/ContactoController.java
+++ src/main/java/com/example/demo/controllers/ContactoController.java
@@ -5,26 +5,20 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.BreadcrumbItem;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
-
-
 @Controller
-@RequestMapping("/grupo21")
-public class HomeController {
-
-    @GetMapping("/")
-    public String paginaInicio(Model model) {
-        setBreadcrumbs(model,"Inicio");
-        return "index";
+@RequestMapping("/grupo21/contacto")
+public class ContactoController {
+    @GetMapping("")
+    public String paginaContacto(Model model){
+        setBreadcrumbs(model,"Contacto");
+        return "contacto";
     }
-    
+
     private void setBreadcrumbs(Model model, String currentPage){
         List<BreadcrumbItem> breadcrumbs = new ArrayList<>();
         breadcrumbs.add(new BreadcrumbItem("Inicio", "/"));
--- src/main/java/com/example/demo/controllers/desarrolloController.java
+++ src/main/java/com/example/demo/controllers/desarrolloController.java
@@ -5,26 +5,21 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.BreadcrumbItem;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
-
-
 @Controller
-@RequestMapping("/grupo21")
-public class HomeController {
+@RequestMapping("/grupo21/desarrollo")
+public class desarrolloController {
 
-    @GetMapping("/")
-    public String paginaInicio(Model model) {
-        setBreadcrumbs(model,"Inicio");
-        return "index";
+    @GetMapping("")
+    public String paginaDesarrollo(Model model){
+        setBreadcrumbs(model,"Desarrollo");
+        return "desarrollo";
     }
-    
+
     private void setBreadcrumbs(Model model, String currentPage){
         List<BreadcrumbItem> breadcrumbs = new ArrayList<>();
         breadcrumbs.add(new BreadcrumbItem("Inicio", "/"));
@@ -32,4 +27,5 @@ public class HomeController {
 
         model.addAttribute("breadcrumbs", breadcrumbs);
     }
+
 }
